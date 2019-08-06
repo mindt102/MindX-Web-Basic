d876399a6435a72b85d1c596631538eda4600d87
minhdt102 <minhdt24082002@gmail.com>
2019-08-06 18:57:55 +0700

    .'

--- Session20/Marvel_characters/app.js
+++ Session20/Marvel_characters/app.js
@@ -2,6 +2,20 @@
 var url = "https://gateway.marvel.com:443/v1/public/characters"
 var publicKey = "cfc8dffa6c6b8ca34753d29d2fad49a9"
 var privateKey = "0f31eb1be795ab4dd4bba01479df9ab77a81bf63"
+var limit = 30
+
+
+function openDetail() {
+    var character_div = document.getElementsByClassName("character")
+    for (i=0;i<character_div.length;i++) {
+        var character = character_div[i]
+        character.addEventListener("click",function (e) {
+            var div_clicked = e.target.parentNode
+            var id = div_clicked.id 
+            window.open(`detail.html?id=${id}`,"_self")         
+        }) 
+    }
+}
 
 function renderCharacters (characters) {
     var content = document.getElementById("content")
@@ -12,10 +26,10 @@ function renderCharacters (characters) {
         var imgSrc = character.thumbnail.path + "." + character.thumbnail.extension
         var name = character.name
         var comicsAvailable = character.comics.available
-        
+        var id = character.id
         //HTML, => content
         var characterHTML = `
-        <div class="character">
+        <div class="character" id=${id}>
             <img src="${imgSrc}" alt="" class="image">
             <h2 class="name">${name}</h2>
             <h3 class="comics">Comics: ${comicsAvailable}</h3>
@@ -23,12 +37,13 @@ function renderCharacters (characters) {
         `
         content.insertAdjacentHTML("beforeend",characterHTML)
     }
+    openDetail()
 }
 
 function fetchCharacter () {
     //2. Generate key, key + url => full url
     var key = marvelKey(privateKey,publicKey)
-    var fullUrl = `${url}?${key}`
+    var fullUrl = `${url}?${key}&limit=${limit}`
 
     //3. Send request, handle response data
     sendGetRequest(fullUrl, function (responseData) {
@@ -36,38 +51,42 @@ function fetchCharacter () {
         renderCharacters(characters)
     })
 }
+function search() {
+    var content = document.getElementById("content")
+    var searchBar = document.getElementById("search_bar")
+    var searchString = searchBar.value
+    var key = marvelKey(privateKey, publicKey)
+    var fullUrl = `${url}?${key}&limit=${limit}`
+    var loading_indicator = '<div id="loader_container"><div class="loader"></div></div>'
+    content.textContent = ""
+    content.insertAdjacentHTML("beforeend",loading_indicator)
+    console.log("searching")
+
+    if (searchString != "") {
+        fullUrl += `&nameStartsWith=${searchString}`
+    }
+    sendGetRequest(fullUrl, function(responseData) {
+        var characters = responseData.data.results
+        renderCharacters(characters)
+        if (content.textContent == "") {
+            content.textContent = "No result"
+        }
+    })
+}
 
 function setupEvents () {
     var btnSearch = document.getElementById("btn_search")
-    btnSearch.addEventListener("click",function(e) {
-        var searchBar = document.getElementById("search_bar")
-        var searchString = searchBar.value
-        var key = marvelKey(privateKey, publicKey)
-        var fullUrl = `${url}?${key}`
-        if (searchString != "") {
-            fullUrl += `&nameStartsWith=${searchString}`
-        }
-        sendGetRequest(fullUrl, function(responseData) {
-            var characters = responseData.data.results
-            renderCharacters(characters)
-        })        
-    })  
+    var searchBar = document.getElementById("search_bar")
+    btnSearch.addEventListener("click",function() {search()})
+    searchBar.addEventListener("keypress",function(e){
+        if(e.keyCode === 13) {search()}
+    })      
 }
 
-function returnDetail() {
-    var character_div = document.getElementsByClassName("character")
-    console.log(character_div)
-    // character_div[0].addEventListener("click",function (e) {
-    //     // console.log("clicked")
-    //     // open("detail.html","_self")
-    //     // return e.target
-    //     console.log(e.target)
-    // })
-}
+
 
 fetchCharacter()
 setupEvents()
 
-var character_divs = document.getElementsByClassName("character")
-console.log(character_divs)
-console.log(character_divs[0])
+
+
--- Session20/Marvel_characters/detail.js
+++ Session20/Marvel_characters/detail.js
@@ -1,1 +1,96 @@
-console.log("hello")
+var id= window.location.search.substring(4)
+var url = `https://gateway.marvel.com:443/v1/public/characters/${id}`
+var publicKey = "cfc8dffa6c6b8ca34753d29d2fad49a9"
+var privateKey = "0f31eb1be795ab4dd4bba01479df9ab77a81bf63"
+var limit = 30
+
+function renderCharacter (character) {
+        var content = document.getElementById("dt_content")
+        content.textContent = ""
+        // link image, name, comics
+        var imgSrc = character.thumbnail.path + "." + character.thumbnail.extension
+        
+        var name = character.name
+        var description = character.description
+        
+        var comics = character.comics
+        var comicsAvailable = comics.available
+        var comicsItems = comics.items
+        var relatedComics = ""
+
+        var events = character.events
+        var eventsAvailable = events.available
+        var eventsItems = events.items
+        var relatedEvents = ""
+        
+        var ytlink = `https://www.youtube.com/results?search_query=${name.replace(" ","+")}+marvel`
+        var wklink = `https://en.wikipedia.org/wiki/Special:Search?search=${name.replace(" ","+")}+marvel&go=Go&ns0=1`
+        var ytref = `<a href="${ytlink}" target="#"><img src="youtube.png" class="logo"></a>`
+        var wkref = `<a href="${wklink}" target="#"><img src="wikipedia.png" class="logo"></a>`
+        
+        if (description == "") {
+            description = "No description"
+        }
+        
+        if (comicsAvailable == 0) {
+            relatedComics = "No comic"
+        }
+        else {
+            for (i=0;i<Math.min(comicsAvailable,5);i++) {
+                relatedComics += `<div class="dt_infos font_size_20">${comicsItems[i].name}</div>`
+            }
+        }
+        
+
+        if (eventsAvailable == 0) {
+            relatedEvents = "No events"
+        }
+        else {
+            for (i=0;i<Math.min(eventsAvailable,5);i++) {
+                relatedEvents += `<div class="dt_infos font_size_20">${eventsItems[i].name}</div>`
+            }
+        }
+
+        //HTML, => content
+        var characterHTML = `
+        <img src="${imgSrc}" id="dt_img">
+        <div id="infos">
+            <div>
+                <h1>${name}</h1>
+                <div class="font_size_20">${description}</div>
+            </div>
+            <div>
+                <h3>Related comics</h3>
+                <div class="related_infos font_size_20">${relatedComics}</div>
+            </div>
+            <div>
+                <h3>Related events</h3>
+                <div class="related_infos font_size_20">${relatedEvents}</div>
+            </div>
+            <div>
+                <h3>References</h3>
+                <div id="logos">
+                    <div>${ytref}</div> 
+                    <div>${wkref}</div>
+                </div>
+            </div>
+        </div>
+        `
+        content.insertAdjacentHTML("beforeend",characterHTML)
+}
+
+
+function fetchCharacter () {
+    //2. Generate key, key + url => full url
+    var key = marvelKey(privateKey,publicKey)
+    var fullUrl = `${url}?${key}&limit=${limit}`
+
+    //3. Send request, handle response data
+    sendGetRequest(fullUrl, function (responseData) {
+        var character = responseData.data.results[0]
+        renderCharacter(character)
+        // console.log(character)
+    })
+}
+
+fetchCharacter()
